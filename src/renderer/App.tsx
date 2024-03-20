336183201df2f29df60b65199b263502ec2564c5
Vivek K J <vivekkj2004@gmail.com>
2024-03-20 20:01:51 +0530

    Add and config Tailwind CSS

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,19 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
 import './App.css';
-
-function Hello() {
-  return (
-    <div>
-      <h1>Avittathur Palli Project ⛪</h1>
-    </div>
-  );
-}
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/renderer/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <div>
+        <h2 className="font-bold">Holy Family Church</h2>
+        <p>Avittathur</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
