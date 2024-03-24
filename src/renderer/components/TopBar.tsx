39c78e06606339b080087532c4ad9f1a46d49ca2
Vivek K J <vivekkj2004@gmail.com>
2024-03-24 12:54:53 +0530

    Add a go back button on non-root pages

--- src/renderer/components/TopBar.tsx
+++ src/renderer/components/TopBar.tsx
@@ -1,8 +1,30 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import backIcon from '../../img/chevron-left-solid.svg';
+
 function TopBar() {
+  const location = useLocation();
+  const { pathname } = location;
+  const navigate = useNavigate();
   return (
     <div className="bg-bgOther px-10 py-10 rounded-xl text-textPrimary">
-      <h2 className="font-extrabold text-2xl">Holy Family Church</h2>
-      <p className="font-medium">Avittathur</p>
+      <div className="flex gap-20 cursor-pointer">
+        <button
+          type="button"
+          className={`flex flex-col justify-center items-center ring-2 rounded-xl ring-textPrimary p-2 ${
+            pathname !== '/' ? 'flex' : 'hidden'
+          }`}
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <img height={20} width={20} src={backIcon} alt="" />
+          <h2 className="font-semibold text-xs">Go back</h2>
+        </button>
+        <div>
+          <h2 className="font-extrabold text-2xl">Holy Family Church</h2>
+          <p className="font-medium">Avittathur</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/renderer/pages/Certificates.tsx
+++ src/renderer/pages/Certificates.tsx
@@ -1,3 +1,9 @@
+import TopBar from '../components/TopBar';
+
 export default function Certificates() {
-  return <div>Certificates</div>;
+  return (
+    <div className="ring-4 ring-bgSecondary p-10 rounded-xl">
+      <TopBar />
+    </div>
+  );
 }
