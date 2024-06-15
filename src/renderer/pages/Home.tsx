813515b04eee110ad2cd1667a88a8ed6d3b8fffc
LeonardkB <123649620+leonardkb@users.noreply.github.com>
2024-06-15 10:58:50 +0530

    Update Home.tsx

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -1,13 +1,20 @@
 import Footer from '../components/Footer';
 import MenuChip from '../components/MenuChip';
 import TopBar from '../components/TopBar';
-
+import Chruch from '../../img/church.png'
+import cloud from '../../img/cloud.png'
 function Home() {
   return (
     <div className="ring-4 ring-bgSecondary p-10 rounded-xl">
       <TopBar />
       <div className="pt-6">
-        <h2 className="text-textPrimary font-bold text-2xl">Church Services</h2>
+        <div className='flex  gap-3'>
+      <img src={Chruch}/>
+        <h2 className="text-textPrimary font-bold text-2xl">
+          Church Services 
+          
+        </h2>
+        </div> 
         <div className="py-10 grid grid-cols-1 md:grid-cols-2 md:gap-x-40 gap-10">
           <MenuChip
             title="Normal Form"
@@ -36,6 +43,8 @@ function Home() {
           />
         </div>
         <div className="py-4">
+          <div className='flex  gap-4'>
+            <img className='w-6 h-5' src={cloud}/>
           <h2 className="text-textPrimary font-bold text-2xl">
             Cloud Services
           </h2>
