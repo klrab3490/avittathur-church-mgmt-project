56b340fae6349b58eb8823284529d0e7c9a06d85
LeonardkB <123649620+leonardkb@users.noreply.github.com>
2024-06-15 10:52:41 +0530

    Update MenuChip.tsx

--- src/renderer/components/MenuChip.tsx
+++ src/renderer/components/MenuChip.tsx
@@ -8,7 +8,7 @@ interface MenuChipProps {
 function MenuChip({ title, route, description }: MenuChipProps) {
   return (
     <Link to={route}>
-      <div className="cursor-pointer px-10 py-6 rounded-lg shadow-lg hover:bg-bgHover hover:transition-colors hover:text-white">
+      <div className="relative px-10 py-6 rounded-lg shadow-lg cursor-pointer hover:bg-bgHover hover:text-white transition-colors">
         <h2 className="text-xl font-bold">{title}</h2>
         <p>{description}</p>
       </div>
