ff0ad5a5e32ac5991b2af3f92bcc85cbe9111a82
Rab Lap <rahulbushi69@gmail.com>
2024-06-15 11:10:27 +0530

    Updated: Main Code Error

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -1,20 +1,20 @@
 import Footer from '../components/Footer';
 import MenuChip from '../components/MenuChip';
 import TopBar from '../components/TopBar';
-import Chruch from '../../img/church.png'
-import cloud from '../../img/cloud.png'
+import Chruch from '../../img/church.png';
+import cloud from '../../img/cloud.png';
+
 function Home() {
   return (
     <div className="ring-4 ring-bgSecondary p-10 rounded-xl">
       <TopBar />
       <div className="pt-6">
-        <div className='flex  gap-3'>
-      <img src={Chruch}/>
-        <h2 className="text-textPrimary font-bold text-2xl">
-          Church Services 
-          
-        </h2>
-        </div> 
+        <div className="flex gap-3">
+          <img alt="Church Icon" src={Chruch} />
+          <h2 className="text-textPrimary font-bold text-2xl">
+            Church Services
+          </h2>
+        </div>
         <div className="py-10 grid grid-cols-1 md:grid-cols-2 md:gap-x-40 gap-10">
           <MenuChip
             title="Normal Form"
@@ -43,11 +43,12 @@ function Home() {
           />
         </div>
         <div className="py-4">
-          <div className='flex  gap-4'>
-            <img className='w-6 h-5' src={cloud}/>
-          <h2 className="text-textPrimary font-bold text-2xl">
-            Cloud Services
-          </h2>
+          <div className="flex gap-4">
+            <img className="w-6 h-5" src={cloud} alt="Cloud Icon" />
+            <h2 className="text-textPrimary font-bold text-2xl">
+              Cloud Services
+            </h2>
+          </div>
           <div className="py-4 grid grid-cols-1 md:grid-cols-2 md:gap-x-40 gap-10">
             <MenuChip
               title="Sync to Cloud"
